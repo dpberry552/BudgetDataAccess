a47cd2341b18fe3e6e7118b334ec59e4d597a0b1
dpberry <dpberry178@gmail.com>
2019-01-02 06:04:21 -0500

    Adding attributes to TransCatRlship class

---
+++ BudgetDataAccess/Models/TransCatRlship.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.Contrib.Extensions;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetDataAccess.Models
+{
+    [Table("TransCatRlship")]
+    public class TransCatRlship : BusinessObject
+    {
+        public int TransID { get; set; }
+        public int CatID { get; set; }
+        public int SubCatID { get; set; }
+        public decimal Amount { get; set; }
+    }
+}

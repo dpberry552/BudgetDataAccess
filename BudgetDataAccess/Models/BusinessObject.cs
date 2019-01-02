d377e5ce74c2c1cc12f5429c25c4908f50c59e7d
dpberry <dpberry178@gmail.com>
2019-01-02 06:03:45 -0500

    Adding methods to BusinessObject class

--- BudgetDataAccess/Models/BusinessObject.cs
+++ BudgetDataAccess/Models/BusinessObject.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper.Contrib.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,16 @@ namespace BudgetDataAccess.Models
     {
         [Key]
         public int ID { get; set; }
+
+        public static T GetById<T>(IDbConnection db, int id)
+            where T : BusinessObject
+        {
+            return db.Get<T>(id);
+        }
+
+        public static void Persist<T>(IDbConnection db, T t)
+        {
+            //TODO
+        }
     }
 }

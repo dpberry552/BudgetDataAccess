7c2735b723e169690168564c7ba16901eb9efb5a
dpberry552 <dpberry178@gmail.com>
2019-03-16 19:35:16 -0400

    add persist method

--- BudgetDataAccess/Models/BusinessObject.cs
+++ BudgetDataAccess/Models/BusinessObject.cs
@@ -8,20 +8,33 @@ using System.Threading.Tasks;
 
 namespace BudgetDataAccess.Models
 {
-    public class BusinessObject
+    public class BusinessObject<T>
+        where T : BusinessObject<T>
     {
         [Key]
-        public int ID { get; set; }
+        public int? Id { get; set; }
+        [Write(false)]
+        public DateTime CreateDate { get; set; }
+        [Write(false)]
+        public DateTime UpdateDate { get; set; }
 
-        public static T GetById<T>(IDbConnection db, int id)
-            where T : BusinessObject
+        public static BusinessObject<T> GetById(IDbConnection db, int id)
         {
             return db.Get<T>(id);
         }
 
-        public static void Persist<T>(IDbConnection db, T t)
+        public static void Persist(IDbConnection db, T t)
         {
-            //TODO
+            t.UpdateDate = DateTime.Now;
+            if(t.Id != null)
+            {
+                db.Update<T>(t);
+            }
+            else
+            {
+                t.CreateDate = DateTime.Now;
+                db.Insert<T>(t);
+            }
         }
     }
 }

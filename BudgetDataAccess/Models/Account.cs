f6615c6e3048a655f449a9c0159791d1e77b7a95
dpberry552 <dpberry178@gmail.com>
2020-02-29 21:42:40 -0500

    method to get accounts for user

--- BudgetDataAccess/Models/Account.cs
+++ BudgetDataAccess/Models/Account.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper.Contrib.Extensions;
+using Dapper;
 
 namespace BudgetDataAccess.Models
 {
     [Table("Account")]
     public class Account : BusinessObject<Account>
     {
+        public string UserId { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
         public string Institution { get; set; }
         public double Balance { get; set; }
+
+        public static IEnumerable<Account> GetByUserId(IDbConnection db, int userId)
+        {
+            var accountList = db.Query<Account>(@"select * from Account where UserId = @id", new { id = userId });
+            return accountList;
+        }
     }
+
 }
